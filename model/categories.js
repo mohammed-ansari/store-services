6687cf47b0b03aa4e15894214fdae5a57beda104
thundercat95 <firozansari517@gmail.com>
2018-03-10 22:36:36 +0530

    json and model changes

--- model/categories.js
+++ model/categories.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var categories = mongoose.model('categories', {
     "name": {type: String},
     "storage": {type: Array},
-    "color":{type: Array}
+    "color":{type: Array},
+    "_product":[{ type: mongoose.Schema.Types.ObjectId, ref: 'products' }]
   });
 
   module.exports = categories;
--- model/products.js
+++ model/products.js
@@ -5,7 +5,7 @@ var products = mongoose.model('products',
     {
         "name": { type: String },
         "price": { type: String },
-        "_category": { type: mongoose.Schema.Types.ObjectId, ref: 'categories' }
+        "_category": [{ type: mongoose.Schema.Types.ObjectId, ref: 'categories' }]
     });
 
 module.exports = products;
